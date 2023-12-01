eec017d3a28c8bda6856281b37147d7c1ab0eb6f
Muhammed Salih Altun <muhammedsalihaltun@gmail.com>
2023-12-01 16:30:40 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -365,7 +365,7 @@ vim.defer_fn(function()
         init_selection = '<c-space>',
         node_incremental = '<c-space>',
         scope_incremental = '<c-s>',
-        node_decremental = '<M-space>',
+        node_decremental = '<bs>',
       },
     },
     textobjects = {
